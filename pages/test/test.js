65f872b6a254f61e51c12a4a0c227e86137ed0e5
华驰科技 <1061401072@qq.com>
2018-11-29 10:53:16 +0800

    Initial Commit

---
+++ pages/test/test.js
@@ -0,0 +1,178 @@
+// pages/test/test.js
+const util = require('../../utils/util.js')
+const recorderManager = wx.getRecorderManager()
+const innerAudioContext = wx.createInnerAudioContext()
+let recordTimeInterval
+Page({
+
+  data: {
+    recorddata:[],
+    voiceDetail:{
+     voiceUrl:'',
+     recordtime:''
+    }
+  },
+
+
+  /**  
+   * 生命周期函数--监听页面加载  
+   */
+  onLoad: function (options) {
+
+    console.log(recorderManager)
+
+
+
+  },
+
+  //开始录音的时候
+  start: function () {
+    const that = this;
+    let recordTime = 0
+    recordTimeInterval = setInterval(function () {
+      recordTime += 1
+      that.setData({
+        'voiceDetail.recordtime': util.formatDayTime(recordTime),
+      })
+      console.log(that.data.voiceDetail);
+    }, 1000)
+    //https://www.cnblogs.com/danielWise/p/9020884.html
+    const options = {
+      duration: 10000,//指定录音的时长，单位 ms
+      sampleRate: 16000,//采样率
+      numberOfChannels: 1,//录音通道数
+      encodeBitRate: 96000,//编码码率
+      format: 'mp3',//音频格式，有效值 aac/mp3
+      frameSize: 50,//指定帧大小，单位 KB
+    }
+    //开始录音--旧版
+    wx.startRecord({
+      success: function (res) {
+        var tempFilePath = res.tempFilePath
+        that.setData({
+          'voiceDetail.voiceUrl': tempFilePath,
+          recorddata: that.data.recorddata.concat(that.data.voiceDetail)
+        })
+        //that.tip("录音完成")
+      },
+      fail: function (res) {
+        //录音失败
+        console.log(res);
+      }
+    })
+    // //开始录音
+    // recorderManager.start(options);
+    // recorderManager.onStart(() => {
+    //   console.log('recorder start')
+    // });
+    // //错误回调
+    // recorderManager.onError((res) => {
+      
+    // })
+  },
+
+  //停止录音
+  stop: function () {
+    clearInterval(recordTimeInterval);
+    var that = this;
+    recorderManager.stop();
+    recorderManager.onStop((res) => {
+      that.setData({
+        'voiceDetail.voiceUrl':res.tempFilePath,
+        recorddata:that.data.recorddata.push(that.data.voiceDetail)
+      })
+       
+      // console.log('停止录音', that.recorddata)
+      // const { tempFilePath } = res
+    })
+  },
+
+
+  //播放声音
+  play: function () {
+    innerAudioContext.src = this.tempFilePath;
+    innerAudioContext.play()
+  
+      // innerAudioContext.onPlay(() => {
+      //   console.log('开始播放')
+      // })
+    innerAudioContext.onError((res) => {
+      console.log(res.errMsg)
+      console.log(res.errCode)
+    })
+  },
+
+  longTap: function () {
+    console.log('longTap....')
+  },
+
+  touchStart: function () {
+    console.log('touchStart....')
+    this.start();
+  },
+
+
+  touchEnd: function () {
+    console.log('touchEnd....')
+    //停止录音--旧版
+    clearInterval(recordTimeInterval);
+    wx.stopRecord()
+    //this.stop()
+  },
+  //播放录音--旧版
+  playRecord: function(e){
+    console.log(e.currentTarget.id);
+    const id = e.currentTarget.id;
+    const src = this.data.recorddata[id].voiceUrl;
+    wx.playVoice({
+      filePath: src,
+      fail: function (res) {
+       console.log(res)
+      }
+    })
+    
+    // innerAudioContext.src = this.data.recorddata[id].voiceUrl;
+    // innerAudioContext.play()
+  },
+  deletelongtap:function(e){
+    var that = this;
+    // const id = e.currentTarget.id;
+    const id = parseInt(e.currentTarget.id);
+    console.log(id);
+    wx.showActionSheet({
+      itemList: ['删除'],
+      success(res) {
+        that.data.recorddata.splice(id, 1)
+        that.setData({
+          recorddata:that.data.recorddata
+        })
+      },
+      fail(res) {
+        console.log(res.errMsg)
+      }
+    })
+  },
+  //发送图片
+  updata_img: function () {
+    var that = this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['compressed'],
+      success: function (res) {
+
+
+        console.log('tempFiles', res.tempFiles[0].size)
+        var size = res.tempFiles[0].size;
+        var size_kb = (size / 1000).toFixed(2);//
+        if (size_kb > 2048) {
+          console.log('2048', '2048')
+        }
+        console.log('size_kb', size_kb)
+      },
+       fail(res) {
+        console.log('图片选中失败', res);
+      }
+    })
+  },
+
+});
---
+++ utils/util.js
@@ -0,0 +1,36 @@
+function formatDayTime(time) {
+  if (typeof time !== 'number' || time < 0) {
+    return time
+  }
+
+  const hour = parseInt(time / 3600, 10)
+  time %= 3600
+  const minute = parseInt(time / 60, 10)
+  time = parseInt(time % 60, 10)
+  const second = time
+
+  return ([hour, minute, second]).map(function (n) {
+    n = n.toString()
+    return n[1] ? n : '0' + n
+  }).join(':')
+}
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+module.exports = {
+  formatTime: formatTime,
+  formatDayTime :formatDayTime
+}
